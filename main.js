207185aa8bfe54b1f75960519f30983181e97ac0
Sandro <layornos@gmail.com>
2019-09-06 23:57:27 +0200

    remove xml serializer

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function xml_to_string(xml_node)
-{
-    if (xml_node.xml)
-        return xml_node.xml;
-    else if (XMLSerializer)
-    {
-        var xml_serializer = new XMLSerializer();
-        return xml_serializer.serializeToString(xml_node);
-    }
-    else
-    {
-        alert("ERROR: Extremely old browser");
-        return "";
-    }
-}
-
 function simulator_specification() {
 
     //
