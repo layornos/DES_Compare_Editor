ac684c5dd08033c707f58953a085a40da2c7e940
Sandro <layornos@gmail.com>
2019-09-23 08:46:52 +0200

    parser for loaded simulator config

--- src/app/configurator/configurator.component.ts
+++ src/app/configurator/configurator.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Simulator } from "../simulator";
+import { Guid } from 'guid-typescript';
+import { SimulationEntity } from '../simulation_entity';
+import { Attribute } from '../attribute';
+import { ReadsAttribute, WritesAttribute, Event } from '../event';
+import { Schedules } from '../schedules';
 
 @Component({
   selector: "app-configurator",
@@ -39,6 +44,7 @@ export class ConfiguratorComponent implements OnInit {
 
   initSimulator() {
     this.simulator = new Simulator();
+    this.simulator.id = Guid.create();
   }
 
   reset(input: boolean) {
@@ -69,11 +75,85 @@ export class ConfiguratorComponent implements OnInit {
   }
 
   loadSimulator() {
+    this.simulator = new Simulator();
     if(this.selectedSimulator != ""){
-      this.simulator = JSON.parse(localStorage.getItem("Simulator: " + this.selectedSimulator));
-      this.simulator.name = this.simulator.name.replace("Simulator: ", "");
+      let simulator = JSON.parse(localStorage.getItem("Simulator: " + this.selectedSimulator));
+      this.simulator.id = simulator["id"];
+      this.simulator.name = simulator.name.replace("Simulator: ", "");
+      this.simulator.description = simulator["description"];
+      for(let entity of simulator["entities"]){
+        let attributes: Attribute[] = [];
+        for(let attr of entity["attributes"]){
+          let tmpAttr: Attribute = {
+            id: Guid.parse(attr["id"]["value"]),
+            type: attr["type"],
+            name: attr["name"]
+          }
+          attributes.push(tmpAttr);
+        }
+        let tmpEntity: SimulationEntity = {
+          id: Guid.parse(entity["id"]["value"]),
+          name: entity["name"],
+          attributes: attributes
+        }
+        this.simulator.entities.push(tmpEntity);
+      }
+      for(let event of simulator["events"]){
+        let reads: ReadsAttribute[] = [];
+        let writes: WritesAttribute[] = [];
+        for(let r of event["reads"]){
+          let tempReads: ReadsAttribute = {
+            attribute: r["attribute"],
+            ofEntity: r["ofEntity"]
+          }
+          reads.push(tempReads);
+        }
+        for(let w of event["writes"]){
+          let tempWrites: WritesAttribute = {
+            attribute: w["attribute"],
+            ofEntity: w["ofEntity"],
+            condition: w["condition"]
+          }
+          writes.push(tempWrites);
+        }
+        let tmpEvent:Event = {
+          id: Guid.parse(event["id"]["value"]),
+          name: event["name"],
+          schedules: event["schedules"],
+          reads: reads,
+          writes: writes
+        }
+        this.simulator.events.push(tmpEvent);
+      }
+      for(let sched of simulator["schedules"]){
+        console.log(Guid.raw());
+        console.log(sched["id"]["value"]);
+        console.log(Guid.isGuid(Guid.parse(sched["id"]["value"])));
+        let tmpSched: Schedules = {
+          id: Guid.parse(sched["id"]["value"]),
+          name: sched["name"],
+          condition: sched["condition"],
+          delay: sched["delay"],
+          start_event: sched["start_event"],
+          end_event: sched["end_event"]
+        }
+        this.simulator.schedules.push(tmpSched);
+      }
+      
     }
   }
+
+  parseObject(obj)
+{
+   for(var key in obj)
+   {
+      console.log("key: " + key + ", value: " + obj[key])
+      if(obj[key] instanceof Object)
+      {
+        this.parseObject(obj[key]);
+      }
+   }
+}
 
   clearLocalStorage(){
     localStorage.clear();
